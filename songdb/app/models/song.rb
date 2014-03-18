fa481140d031beea8266f26034506a6a688e8e0e
Renato Arruda <rarruda0@gmail.com>
2014-03-18 22:25:20 +0100

    - song: require songs to have a language set.

--- songdb/app/models/song.rb
+++ songdb/app/models/song.rb
@@ -3,6 +3,8 @@ class Song < ActiveRecord::Base
 	belongs_to :language
 
 	validates :title, :presence => true
+	validates :language_id, :presence => true
+
 	accepts_nested_attributes_for :verses, :allow_destroy => true
 	  #reject_if => lambda { |a| a[:content].blank? }
 end
