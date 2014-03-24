9fecd58a0fe8b3ef7ce4e9f1048d8ea16e5b8cd0
Renato Arruda <rarruda0@gmail.com>
2014-03-24 23:33:30 +0100

    - slightly fancier sorting of verses

--- songdb/app/models/song.rb
+++ songdb/app/models/song.rb
@@ -1,10 +1,10 @@
 class Song < ActiveRecord::Base
-	has_many :verses, :dependent => :destroy, order: 'position ASC'
+	has_many :verses, -> { order(position: :asc, id: :asc) }, :dependent => :destroy
 	belongs_to :language
 
 	validates :title, :presence => true
 	validates :language_id, :presence => true
 
 	accepts_nested_attributes_for :verses, :allow_destroy => true
-	  #reject_if => lambda { |a| a[:content].blank? }
+    #:reject_if => lambda { |a| a[:content].blank? },
 end
