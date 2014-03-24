df8c44219e7ba9c0bcac3006d17f263eba4d42ab
Renato Arruda <rarruda0@gmail.com>
2014-03-24 23:32:43 +0100

    - bugfix: now can add verses when adding new songs: removed dependency on song_id on verse model, so that its now possible to add songs and verses from the initial new song form.

--- songdb/app/models/verse.rb
+++ songdb/app/models/verse.rb
@@ -2,5 +2,5 @@ class Verse < ActiveRecord::Base
 	belongs_to :song
 	belongs_to :verse_type
 
-	validates :song_id,:verse_type_id,:content, :presence => true
+	validates :verse_type_id,:content, :presence => true
 end
